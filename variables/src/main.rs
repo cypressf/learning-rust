dcb4e49379b4563cef2727e00ce1bfa823cfc3fd
Cypress Frankenfeld <iamcypress@gmail.com>
2018-11-12 21:38:56 -0500

    Add example of shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -9,6 +9,16 @@ fn main() {
     let y = 10;
     println!("The value of y is {} and cannot change", y);
 
+    // a shadowed variable
+    let y = y * 4 + 2;
+    println!("The value of y was redeclared and shadowed as {} now", y);
+
+    // shadowed variables can change type
+    let spaces = "     ";
+    let spaces = spaces.len();
+    println!("`spaces` changed from a string to a count ({})", spaces);
+
+
     // a constant
     const Z: u32 = 100_000;
     println!("The value of Z is {} and is fixed at compile time", Z);
