4464314a282ccfdd188b4cf1709e6576bec903ea
Cypress Frankenfeld <iamcypress@gmail.com>
2018-11-12 20:41:11 -0500

    Add guessing game exercise

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,19 @@
+extern crate rand;
+
+use rand::Rng;
+use std::io;
+
+fn main() {
+    println!("Guess the number");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is {}", secret_number);
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+    	.expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+}
