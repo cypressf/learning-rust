f83c123fbe1c3970144dafa8473dd5f110e10d28
Cypress Frankenfeld <iamcypress@gmail.com>
2020-04-12 17:19:25 -0400

    Create initial rectangle program, without structs

---
+++ rectangles/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let width1 = 30;
+    let height1 = 30;
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(width1, height1)
+    );
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
+}
