b4ed0bc425baa9036e254dba424c31d731aa322a
Cypress Frankenfeld <iamcypress@gmail.com>
2020-04-22 09:12:35 -0400

    Add a associated function

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -8,19 +8,40 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn fits_in(&self, rectangle: &Rectangle) -> bool {
+        self.width < rectangle.width && self.height < rectangle.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size}
+    }
 }
 
 fn main() {
-    let rectangle = Rectangle {
+    let rectangle1 = Rectangle {
         width: 30,
         height: 30
     };
 
+    let rectangle2 = Rectangle {
+        width: 1,
+        height: 29
+    };
+
+    // associated functions use ::
+    let square = Rectangle::square(5);
+
+    // methods use .
     println!(
         "The area of the rectangle is {} square pixels.",
-        rectangle.area()
+        rectangle1.area()
     );
 
-    // we still have ownership of rectangle
-    println!("{:?}", rectangle);
+    println!(
+        "Rectangle: {:?} fits inside rectangle {:?}?\n {}",
+        rectangle2, rectangle1, rectangle2.fits_in(&rectangle1)
+    );
+
+    println!("Square: {:?}", square);
 }
