82092fce574f5dc76d7723783c0f07e135069e54
Cypress Frankenfeld <iamcypress@gmail.com>
2020-04-22 11:28:21 -0400

    Add examples of invalid acess

---
+++ privacy/src/lib.rs
@@ -0,0 +1,24 @@
+mod outer {
+    pub fn middle() {
+        println!("Middle")
+    }
+    fn middle_private() {
+        println!("Middle private")
+    }
+
+    pub mod inner {
+        pub fn inner() {
+            println!("Inner")
+        }
+        fn inner_private() {
+            println!("Inner private")
+        }
+    }
+}
+
+fn try_calling() {
+    outer::middle(); // success
+    outer::middle_private(); // failure
+    outer::inner::inner(); // success
+    outer::inner::inner_private(); // failure
+}
