92c5ddfab0a67a6e52ae6b9fd1ed56d3ec23c386
Cypress Frankenfeld <iamcypress@gmail.com>
2020-04-21 21:39:54 -0400

    Use method instead of function

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,6 +4,12 @@ struct Rectangle {
     height: u32
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
     let rectangle = Rectangle {
         width: 30,
@@ -12,13 +18,9 @@ fn main() {
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rectangle)
+        rectangle.area()
     );
 
     // we still have ownership of rectangle
     println!("{:?}", rectangle);
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
 }
