6af30364c59eb25c9edf3b87c0a3712dea121e01
Cypress Frankenfeld <iamcypress@gmail.com>
2022-07-07 15:36:27 -0400

    Make basic server

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,5 +1,7 @@
 use std::{
+    fs::File,
     io::Read,
+    io::Write,
     net::{TcpListener, TcpStream},
 };
 
@@ -14,6 +16,19 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
+
+    let root = b"GET / ";
     stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer));
+
+    let (response_code, file) = match buffer.starts_with(root) {
+        true => ("200 OK", "hello.html"),
+        false => ("404 NOT FOUND", "404.html"),
+    };
+    let mut file = File::open(file).unwrap();
+    let mut file_string = String::new();
+    file.read_to_string(&mut file_string).unwrap();
+    stream
+        .write(format!("HTTP/1.1 {}\r\n\r\n{}", response_code, file_string).as_bytes())
+        .unwrap();
+    stream.flush().unwrap();
 }
