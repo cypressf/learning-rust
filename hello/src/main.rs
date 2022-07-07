8decb19bb9c9ef9e4adfdc76e81f916e3395bd61
Cypress Frankenfeld <iamcypress@gmail.com>
2022-07-07 17:28:00 -0400

    Spawn one thread per connection

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -12,7 +12,8 @@ fn main() {
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+
+        thread::spawn(|| handle_connection(stream));
     }
 }
 
