8b2b44174dc268c57ac3614d0d85542fa656c0ed
Cypress Frankenfeld <iamcypress@gmail.com>
2022-07-19 17:54:38 -0400

    Implement the rest of the thread pool

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,6 +1,32 @@
-use std::thread::Thread;
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
 
-pub struct ThreadPool;
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+pub struct ThreadPool {
+    sender: mpsc::Sender<Job>,
+    workers: Vec<Worker>,
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            println!("Worker {} got a job; executing.", id);
+            job();
+        });
+
+        Worker { id, thread }
+    }
+}
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -10,12 +36,20 @@ impl ThreadPool {
     /// The `new` function will panic if the size is zero.
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
-        unimplemented!()
+        let mut workers = Vec::with_capacity(size);
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        for id in 0..size {
+            workers.push(Worker::new(id, receiver.clone()));
+        }
+        ThreadPool { sender, workers }
     }
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
-        unimplemented!()
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
     }
 }
